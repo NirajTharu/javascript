adbf902cbe27a7b4c0952f588b90adc26f64a1dd
NirajTharu <nirajchaudhary@Book.local>
2025-05-23 18:44:42 +0545

    indec.html file was updated and practice set 4 was compleated

--- practiceset4.js
+++ practiceset4.js
@@ -5,5 +5,17 @@
 // 50 - 59, D
 // 0 - 49, F
 
-
- 
+{
+  let marks_obtain = prompt("Enter the marks you have obtain:");
+  if (marks_obtain >= 90 && marks_obtain <= 100) {
+    console.log(marks_obtain, "You got grade A.");
+  } else if (marks_obtain >= 70 && marks_obtain <= 89) {
+    console.log(marks_obtain, "You got grade B.");
+  } else if (marks_obtain >= 60 && marks_obtain <= 69) {
+    console.log(marks_obtain, "You got grade C.");
+  } else if (marks_obtain >= 50 && marks_obtain <= 59) {
+    console.log(marks_obtain, "You got grade D.");
+  } else {
+    console.log(marks_obtain >= 0 && marks_obtain <= 49, "You got grade F.");
+  }
+}
