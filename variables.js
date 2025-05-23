612788398d326402b82be905faf55994c9b6aad2
NirajTharu <nirajchaudhary@Book.local>
2025-05-23 16:17:22 +0545

    First files of the javascript

---
+++ variables.js
@@ -0,0 +1,27 @@
+// Variables are the containers for data storage
+
+// variables Rues:
+/* 1. Variables names are case sensitive: 'a' & 'A' are different 
+   2. Only letters, digits, underscore(_) and $ sign is allowed. (not even space)
+   3. Only letters, underscore(_) and $ is allowed to be the first character.
+   4. Reserved words cannot be the variables name.
+   */
+
+/* For Naming convention:
+fullName: The first letter of 1st word is small and 1st letter of the second letter is capital. This is called "Camel case" 
+fullname:
+full_name: This is called "snake case".
+full-name: This is called 'Kebab case'
+FullName: This is called "Pascal Case"*/
+
+/* For the declaration of the variables:
+var: Variables can be re-declared & updated. A global scope variable.
+let: Variables can not be re-declared but can be updated. A block scope variable. 
+const: Variables can not be redeclared or updated. A block scope variable. */
+
+FullName = "Niraj Tharu";
+age = 34;
+isBoy = true;
+console.log(FullName);
+console.log(age);
+console.log(isBoy);
